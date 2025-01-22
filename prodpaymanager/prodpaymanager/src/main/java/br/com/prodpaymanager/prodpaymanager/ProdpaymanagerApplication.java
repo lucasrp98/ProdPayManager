b1729822e3f453a4fafeda7c30dbd0690bb7c8cf
Lucas Ribeiro <lucaorpacheco@gmail.com>
2025-01-21 22:05:36 -0300

    + commit inicial do projeto

---
+++ prodpaymanager/prodpaymanager/src/main/java/br/com/prodpaymanager/prodpaymanager/ProdpaymanagerApplication.java
@@ -0,0 +1,13 @@
+package br.com.prodpaymanager.prodpaymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdpaymanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdpaymanagerApplication.class, args);
+	}
+
+}
---
+++ prodpaymanager/prodpaymanager/src/test/java/br/com/prodpaymanager/prodpaymanager/ProdpaymanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.prodpaymanager.prodpaymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdpaymanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
